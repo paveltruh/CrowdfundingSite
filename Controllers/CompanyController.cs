2808ca72ce3a68d4cd3a58956fe5be1e4febce34
Pavel Truhan <paveltruh@gmail.com>
2019-07-30 11:12:24 +0300

    update2

---
+++ Controllers/CompanyController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CompanyController : Controller
+    {
+        private readonly UsersContext _usersContext;
+
+        public CompanyController( UsersContext usersContext)
+        {
+            _usersContext = usersContext;
+        }
+
+        public IActionResult Index(string id)
+        {
+            return View();
+        }
+    }
+}
